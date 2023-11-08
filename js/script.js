14ae3d75de7c58a642b1b5461c37cc9e5bd876a8
David Costa <david.antunes.costa@hotmail.com>
2023-11-08 16:18:57 -0300

    Check point
    Mudança geral no site!

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
-
+import SlideNav from './modules/slide.js'
 import initScrollSuave from './modules/scroll-suave.js'
 import initMenuMobile from './modules/menu-mobile.js'
 
 
 initScrollSuave()
 initMenuMobile()
+
+
+const slide = new SlideNav('.slide','.slide-wrapper');
+slide.init();
+slide.addArrow('.prev', '.next')
+slide.addControl('.custom-controls')
